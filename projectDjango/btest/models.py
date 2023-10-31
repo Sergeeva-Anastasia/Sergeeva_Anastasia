899f01b72092fce78c2a1a97e57bf310542964d3
sergeeva_anastasia <sergeevaa130400@gmail.com>
2023-10-31 14:27:15 +0300

    start

--- projectDjango/btest/models.py
+++ projectDjango/btest/models.py
@@ -5,3 +5,7 @@ class Chanells(models.Model):
   content = models.TextField(null=True, blank=True)
   price = models.FloatField(null=True, blank=True)
   published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+from btest.models import Chanells
+b1 = Chanells(title='Россия 1', content='Телеканал «Россия 1» — это информационно-развлекательный телеканал, флагман ВГТРК.')
+
--- projectDjango/btest/views.py
+++ projectDjango/btest/views.py
@@ -1,4 +1,1 @@
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Привет! Это твое первое приложение!")
